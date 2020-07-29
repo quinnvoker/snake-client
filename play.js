8ebde98fc3a0f76ae9d521251453b55beb26b60c
quinnvoker <quinnvoker@gmail.com>
2020-07-29 13:46:09 -0700

    added data handler

--- play.js
+++ play.js
@@ -5,12 +5,14 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: '<IP ADDRESS HERE>',
+    host: '135.23.222.131',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
+  conn.on('data', (data) => {
+    console.log(data);
+  });
   return conn;
 };
 
