e323bbc765f26ec66c705cdccf80e38dd424df4b
quinnvoker <quinnvoker@gmail.com>
2020-07-29 13:56:32 -0700

    separated netcode from play.js, added player initials

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+const { builtinModules } = require('module');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Connected! Welcome to Snake!');
+    conn.write('Name: QNN');
+  });
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = connect;
